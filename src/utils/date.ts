fda6d7f7e3c215bc799c48206621103fbc23b342
Baptiste Adrien <adrien.baptiste@gmail.com>
2023-10-06 10:34:26 +0200

    fix: date and make openai optionnal (#46)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-import { formatDistance } from 'date-fns';
+import { formatDistance, format, parseISO } from 'date-fns';
 
 export const getRelativeDate = (date: string | Date) => {
   const formatDate = typeof date === 'string' ? new Date(date) : date;
@@ -6,4 +6,15 @@ export const getRelativeDate = (date: string | Date) => {
   return formatDistance(formatDate, new Date(), {
     addSuffix: true,
   });
+};
+
+export const formatDate = (
+  date: string | Date,
+  dateFormat = 'MMMM dd, yyyy'
+) => {
+  if (date instanceof Date) {
+    return format(date, dateFormat);
+  }
+
+  return format(parseISO(date), dateFormat);
 };
