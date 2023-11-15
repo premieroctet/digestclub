f332311e9a20891c7c4362d8ccbf753fe46ccfca
quentingrchr <q.grancher@gmail.com>
2023-11-15 18:24:00 +0100

    add dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,89 @@
+import db from '@/lib/db';
 import { MetadataRoute } from 'next';
 const siteUrl = process.env.VERCEL_URL || 'https://digest.club';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+interface TeamData {
+  slug: string;
+  updatedAt: Date;
+}
+
+async function getTeamsData() {
+  const teams = await db.team.findMany({
+    select: {
+      slug: true,
+      Digest: {
+        select: {
+          updatedAt: true,
+        },
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      },
+    },
+  });
+  return teams.map((team) => {
+    return {
+      slug: team.slug,
+      updatedAt: team.Digest[0]?.updatedAt,
+    };
+  });
+}
+
+function getTeamRoutes(teamsData: TeamData[]): MetadataRoute.Sitemap {
+  return teamsData.map(({ slug, updatedAt }) => ({
+    url: `${siteUrl}/teams/${slug}`,
+    lastModified: updatedAt,
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }));
+}
+
+async function getDigestsSlugsOfTeam(teamSlug: string) {
+  return db.digest.findMany({
+    where: {
+      AND: [
+        {
+          team: {
+            slug: teamSlug,
+          },
+        },
+        {
+          NOT: {
+            publishedAt: null,
+          },
+        },
+      ],
+    },
+    select: {
+      slug: true,
+      updatedAt: true,
+    },
+  });
+}
+
+async function getDigestRoutes(teamsData: TeamData[]) {
+  const digestsSiteMap: MetadataRoute.Sitemap = [];
+  const promises = teamsData.map(async ({ slug: teamSlug }) => {
+    const digests = await getDigestsSlugsOfTeam(teamSlug);
+    digests.forEach((digest) => {
+      digestsSiteMap.push({
+        url: `${siteUrl}/${teamSlug}/${digest.slug}`,
+        lastModified: digest.updatedAt,
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      });
+    });
+  });
+
+  await Promise.all(promises);
+  return digestsSiteMap;
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const teamsData = await getTeamsData();
+  const teamRoutes = getTeamRoutes(teamsData);
+  const digestRoutes = await getDigestRoutes(teamsData);
+
   return [
     {
       url: siteUrl,
@@ -12,8 +94,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${siteUrl}/updates`,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.6,
+      changeFrequency: 'monthly',
+      priority: 0.5,
     },
     {
       url: `${siteUrl}/discover`,
@@ -25,13 +107,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${siteUrl}/auth/login`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 0.7,
+      priority: 0.4,
     },
     {
       url: `${siteUrl}/unsubscribe`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
-      priority: 0.7,
+      priority: 0.2,
     },
+    ...teamRoutes,
+    ...digestRoutes,
   ];
 }
