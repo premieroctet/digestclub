089aa5d2b670cbbbf954d2ec2e5468bef5c1a8b3
quentingrchr <q.grancher@gmail.com>
2023-12-07 12:26:53 +0100

    fix responsive

--- src/components/PopularTags.tsx
+++ src/components/PopularTags.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Tag, { ITag } from './Tag';
 import Link from 'next/link';
-import { TagPayload } from '@prisma/client';
 
 interface Props {
   tags: ITag[];
@@ -12,10 +11,10 @@ export default function PopularTags({ tags, currentTag }: Props) {
   return (
     <div className="bg-white p-4 border border-gray-200 rounded-lg">
       <h4 className="text-xl font-bold">Popular tags</h4>
-      <div className="flex gap-4 mt-4">
+      <div className="flex gap-2 mt-4 flex-wrap">
         {tags.map(({ id, name, slug, description }) => (
           <Link key={id} href={`/tags/${slug}`}>
-            <div className="flex flex-col items-start gap-2">
+            <div className="flex flex-col items-start gap-&">
               <Tag
                 tag={{
                   id,
