1b80e37057411d1ed202bebb771fc3c33e7569da
Baptiste Adrien <adrien.baptiste@gmail.com>
2024-06-06 11:22:00 +0200

    feat: switch to gpt-o, fix some issue (#122)

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import redirectionioMiddleware from '@redirection.io/vercel-middleware';
-
-export default redirectionioMiddleware;
-
-export const config = {
-  unstable_allowDynamic: [
-    '/node_modules/@redirection.io/**',
-  ],
-}
