8a9af9f2a8cb8e958b5801262e21b7971d0a01f5
quentingrchr <q.grancher@gmail.com>
2024-10-02 12:14:49 +0200

    refacto: fix route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,6 @@
 import NextAuth from 'next-auth';
 import options from './options';
 
-export const handler = NextAuth(options);
+const handler = NextAuth(options);
 
-export {
-  handler as DELETE,
-  handler as GET,
-  handler as OPTION,
-  handler as POST,
-  handler as PUT,
-};
+export { handler as GET, handler as POST };
