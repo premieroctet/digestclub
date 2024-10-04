28ed6859389c8c19000f74456a57badeaba8c83c
quentingrchr <q.grancher@gmail.com>
2024-10-04 10:33:11 +0200

    refacto teams/teamId/bookmark

--- src/app/api/teams/[teamId]/bookmark/route.ts
+++ src/app/api/teams/[teamId]/bookmark/route.ts
@@ -1,77 +1,59 @@
-import options from '@/app/api/auth/[...nextauth]/options';
+import { checkTeamAppRouter } from '@/lib/middleware';
 import messages from '@/messages/en';
 import { saveBookmark } from '@/services/database/bookmark';
-import { getTeamMembershipById } from '@/services/database/membership';
 import { HandlerApiError, HandlerApiResponse } from '@/utils/handlerResponse';
 import { Bookmark } from '@prisma/client';
 import * as Sentry from '@sentry/nextjs';
-import { getServerSession } from 'next-auth';
 import { createEdgeRouter } from 'next-connect';
 import type { NextRequest } from 'next/server';
 
 export type ApiBookmarkResponseSuccess = Bookmark;
 
-interface RequestContext {
+export interface TeamsRequestContext {
   params: {
     teamId: string;
   };
+
+  // Not the best way to do this but it works for now
+  // We need the middleware to set these values
+  // Right now next-connect doesn't support generics to enrich the request (https://github.com/hoangvvo/next-connect/issues/230)
+  membershipId: string;
+  teamId: string;
+  user: { id: string; email: string };
 }
 
-const router = createEdgeRouter<NextRequest, RequestContext>();
+const router = createEdgeRouter<NextRequest, TeamsRequestContext>();
 
-router
-  .use(async (req, event, next) => {
-    const session = await getServerSession(options);
-    // req.query.state is for Slack integration (we cant choose the name of the query param)
-    // @todo implement slack integration with teamId (req.query.state)
+router.use(checkTeamAppRouter).post(async (req, event, next) => {
+  const body = await req.json();
+  const linkUrl = body.link as string;
+  try {
     const teamId = event.params.teamId as string;
+    const membershipId = event.membershipId as string;
 
-    if (!session && !teamId) {
-      return HandlerApiError.unauthorized();
+    if (!linkUrl) {
+      return HandlerApiError.badRequest();
     }
 
-    const membership = await getTeamMembershipById(teamId, session!.user?.id);
+    const bookmark = await saveBookmark(linkUrl, teamId, membershipId);
+    return HandlerApiResponse.success(bookmark);
+  } catch (error: unknown) {
+    const error_code = (error as TypeError)
+      .message as keyof typeof messages.bookmark.create.error;
 
-    if (!membership) {
-      return HandlerApiError.unauthorized();
-    }
+    Sentry.captureMessage(
+      `Failed to save bookmark. Cause: ${
+        messages.bookmark.create.error[error_code] ??
+        (error as TypeError).message
+      } (${linkUrl})`
+    );
 
-    return next();
-  })
-  .get(async (req, event, next) => {
-    const body = await req.json();
-    const linkUrl = body.link as string;
-    try {
-      const session = await getServerSession(options);
-      const { teamId } = event.params;
-      if (!session || !teamId) {
-        return HandlerApiError.unauthorized();
-      }
-      const membership = await getTeamMembershipById(teamId, session?.user?.id);
+    // eslint-disable-next-line no-console
+    console.log(error);
+    return HandlerApiError.internalServerError();
+  }
+});
 
-      if (!linkUrl) {
-        return HandlerApiError.badRequest();
-      }
-
-      const bookmark = await saveBookmark(linkUrl, teamId, membership?.id);
-      return HandlerApiResponse.success(bookmark);
-    } catch (error: unknown) {
-      const error_code = (error as TypeError)
-        .message as keyof typeof messages.bookmark.create.error;
-
-      Sentry.captureMessage(
-        `Failed to save bookmark. Cause: ${
-          messages.bookmark.create.error[error_code] ??
-          (error as TypeError).message
-        } (${linkUrl})`
-      );
-
-      // eslint-disable-next-line no-console
-      console.log(error);
-      return HandlerApiError.internalServerError();
-    }
-  });
-
-export async function GET(request: NextRequest, ctx: RequestContext) {
+export async function POST(request: NextRequest, ctx: TeamsRequestContext) {
   return router.run(request, ctx) as Promise<Response>;
 }
